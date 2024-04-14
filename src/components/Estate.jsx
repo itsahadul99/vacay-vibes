db34865d562af962ed67bbfebe7e218bd06c887f
itsahadul99 <itsahadul999@gmail.com>
2024-04-14 23:14:18 +0600

    animated estate property section

--- src/components/Estate.jsx
+++ src/components/Estate.jsx
@@ -11,34 +11,33 @@ const Estate = ({ data }) => {
                 <title>Vacay Vibes || Estate</title>
             </Helmet>
             <div className="relative flex h-48 w-full justify-center">
-                <div className="absolute left-4 right-6 top-4 flex items-center justify-between">
-                    {/* love  */}
-                    <div className="flex items-center"></div>
-                    <button className="rounded-xl bg-[#4CCD99] px-3 py-1 font-medium text-white duration-200 hover:hover:bg-[#007F73]">For {status}</button>
+                <div data-aos="fade-up" data-aos-duration="3000" className="absolute left-4 right-6 top-4 flex items-center justify-between">
+                    <div className="flex items-center"><button className="rounded-xl bg-[#4CCD99] px-3 py-1 font-medium text-white duration-200 hover:hover:bg-[#007F73]">Status:</button></div>
+                    <button className="rounded-xl bg-[#4CCD99] px-3 py-1 font-medium text-white duration-200 hover:hover:bg-[#007F73]">{status}</button>
                 </div>
-                <img width={350} className="h-full rounded-lg bg-black/40" src={image} alt="card navigate ui" />
+                <img data-aos ="fade-up" data-aos-duration="2000" width={350} className="h-full rounded-lg bg-black/40" src={image} alt="card navigate ui" />
             </div>
             <div className="flex justify-between items-center font-semibold">
-                <p>Area : {area}</p>
-                <p>Price: {price}</p>
+                <p data-aos="fade-left" data-aos-duration="1800">Area : {area}</p>
+                <p data-aos="fade-left" data-aos-duration="2000">Price: {price}</p>
             </div>
             <div className="mx-auto space-y-2  font-semibold">
-                <div className="flex justify-between items-center text-sm font-medium">
+                <div data-aos="fade-left" data-aos-duration="2200" className="flex justify-between items-center text-sm font-medium">
                     <p className="flex gap-3 items-center"><FaLocationPin /> {location}</p>
                     <p><span className="font-bold">Category:</span> {segment_name}</p>
                 </div>
-                <h6 className="text-sm md:text-lg lg:text-xl">{estate_title}</h6>
+                <h6 data-aos="fade-left" data-aos-duration="3000" className="text-sm md:text-lg lg:text-xl">{estate_title}</h6>
             </div>
             <div>
                 <div>
-                    <h5 className="text-lg font-semibold">Facilities: </h5>
-                    <div className="px-5">
+                    <h5 data-aos="fade-right" data-aos-duration="1800" className="text-lg font-semibold">Facilities: </h5>
+                    <div data-aos="fade-right" data-aos-duration="2200" className="px-5">
                         {
                             facilities.map((item, idx) => <li key={idx}>{item}</li>)
                         }
                     </div>
                 </div>
-                <Link to={`/${id}`}><button className="rounded-lg bg-[#4CCD99] mt-2 px-4 py-2 font-sans font-semibold text-white duration-300 hover:scale-105 hover:bg-[#007F73]">{button}</button></Link>
+                <Link to={`/${id}`}><button data-aos="fade-up" data-aos-duration="2500"  className="rounded-lg bg-[#4CCD99] mt-2 px-4 py-2 font-sans font-semibold text-white duration-300 hover:scale-105 hover:bg-[#007F73]">{button}</button></Link>
             </div>
         </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,12 +11,14 @@ const Home = () => {
         <div className="mb-5 lg:mb-10">
             <Banner />
             <div className="my-5 lg:my-10">
-            <div className="my-5 lg:my-16 flex flex-col justify-center items-center">
-                <MdOutlineKeyboardArrowUp size={40} className="font-bold text-[#4CCD99]" />
-                <h4 className="text-sm lg:text-lg font-medium opacity-70 p-2">Find your dream Property</h4>
-                <h1 className="text-xl uppercase md:text-3xl lg:text-5xl font-normal pb-2">Find a Property</h1>
-                <hr className="bg-[#4CCD99] w-2/6 lg:w-1/6 h-1 "/>
-            </div>
+                <div data-aos="flip-left"
+                    data-aos-easing="ease-out-cubic"
+                    data-aos-duration="2000" className="my-5 lg:my-16 flex flex-col justify-center items-center">
+                    <MdOutlineKeyboardArrowUp size={40} className="font-bold text-[#4CCD99]" />
+                    <h4 className="text-sm lg:text-lg font-medium opacity-70 p-2">Find your dream Property</h4>
+                    <h1 className="text-xl uppercase md:text-3xl lg:text-5xl font-normal pb-2">Find a Property</h1>
+                    <hr className="bg-[#4CCD99] w-2/6 lg:w-1/6 h-1 " />
+                </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-8 justify-evenly items-center w-full my-5 lg:my-10">
                     {
                         data.map((item, idx) => (<Estate key={idx} data={item} />))
